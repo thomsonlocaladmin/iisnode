12a1a179dd1c828b85d66261c7e7ec832ebbcbae
Paul Barroso <paul.barroso@thomsonlocal.com>
2014-02-12 10:05:54 +0000

    tests

--- app/middleware/configuration.js
+++ app/middleware/configuration.js
@@ -5,12 +5,26 @@ var nconf = require('nconf');
 function Config(){
     nconf.argv().env('_');
     var environment = nconf.get('NODE:ENV') || 'development';
-    //TODO: paths fix
-    nconf.file(environment, 'D:/config/' + environment + '.json');
+    var path = "";
+    if (isIISNodeEnvironment()) {
+        path = 'D:/';
+    }
+    nconf.file(environment, path + 'config/' + environment + '.json');
 }
 
 Config.prototype.get = function(key) {
+    if (key === "iisnode") {
+        return isIISNodeEnvironment();
+    }
     return nconf.get(key);
+};
+
+function isIISNodeEnvironment() {
+    var environment = nconf.get('NODE:ENV') || 'development';
+    if (environment === "staging" || environment === "production") {
+        return true;
+    }
+    return false;
 };
 
 module.exports = new Config();
--- app/server.js
+++ app/server.js
@@ -6,9 +6,15 @@ var express = require('express'),
     config = require('./middleware/configuration');
 
 var app = express();
-var port = process.env.PORT;
 
-console.log("test env:  " + config.get('test:key'));
+var port = null;
+if (config.get("iisnode")) {
+    port = process.env.PORT;
+    console.log("running on iisnode");
+} else {
+    port = config.get('express:port');
+    console.log("running without iisnode");
+}
 
 //MIDDLEWARE
 app.set('port', port);
@@ -17,8 +23,11 @@ app.use(express.urlencoded());
 app.use(app.router);
 
 //ROUTES
-//TODO: paths fix
-app.get('/node/iisnode/api/healthcheck', routes.healthcheck.index);
+var path = "";
+if (config.get("iisnode")) {
+    path = '/node/iisnode/api';
+}
+app.get(path + '/healthcheck', routes.healthcheck.index);
 
 //START
 var server = http.createServer(app);
